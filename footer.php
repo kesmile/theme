96300cc3f0fa4ce094476ed43cc2f798f7d1f73c
kelvi s. padilla <kurtkelvisk88@gmail.com>
2014-05-12 21:42:26 -0600

    primer commit

---
+++ footer.php
@@ -0,0 +1,11 @@
+<?php
+	wp_footer();
+?>
+    <script src="<?php bloginfo( 'stylesheet_directory' ); ?>/js/vendor/jquery.js"></script>
+    <script src="<?php bloginfo( 'stylesheet_directory' ); ?>/js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
+
---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+  <head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>temp</title>
+    <link rel="stylesheet" href="<?php bloginfo( 'stylesheet_directory' ); ?>/css/foundation.min.css" />
+    <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+    <script src="<?php bloginfo( 'stylesheet_directory' ); ?>/js/vendor/modernizr.js"></script>
+    <?php wp_head(); ?>
+  </head>
+  <body <?php body_class(); ?>>
+    <div class="row">
+    <header>
+    </header>
